899678d316fc8ecc6b4bd9ea5a726c7488b4dac8
Joseph Fischetti <fischettijw@gmail.com>
2020-07-07 05:21:51 -0400

    Add project files.

---
+++ FrmResizeTextboxAndFont.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace FormResizeTextboxAndFonts
+{
+    partial class FrmResizeTextboxAndFont
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClick = new System.Windows.Forms.Button();
+            this.TxtSingle = new System.Windows.Forms.TextBox();
+            this.TxtMultiline = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClick
+            // 
+            this.btnClick.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClick.Location = new System.Drawing.Point(15, 38);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(100, 40);
+            this.btnClick.TabIndex = 0;
+            this.btnClick.Text = "Click";
+            this.btnClick.UseVisualStyleBackColor = true;
+            // 
+            // TxtSingle
+            // 
+            this.TxtSingle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtSingle.Location = new System.Drawing.Point(144, 44);
+            this.TxtSingle.Name = "TxtSingle";
+            this.TxtSingle.Size = new System.Drawing.Size(150, 29);
+            this.TxtSingle.TabIndex = 1;
+            this.TxtSingle.Text = "Single Line";
+            // 
+            // TxtMultiline
+            // 
+            this.TxtMultiline.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtMultiline.Location = new System.Drawing.Point(322, 44);
+            this.TxtMultiline.Multiline = true;
+            this.TxtMultiline.Name = "TxtMultiline";
+            this.TxtMultiline.Size = new System.Drawing.Size(150, 29);
+            this.TxtMultiline.TabIndex = 2;
+            this.TxtMultiline.Text = "Multiline";
+            // 
+            // FrmResizeTextboxAndFont
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 161);
+            this.Controls.Add(this.TxtMultiline);
+            this.Controls.Add(this.TxtSingle);
+            this.Controls.Add(this.btnClick);
+            this.MinimumSize = new System.Drawing.Size(500, 200);
+            this.Name = "FrmResizeTextboxAndFont";
+            this.Text = "Form Resize Textbox abd Fonts";
+            this.Load += new System.EventHandler(this.FrmResizeTextboxAndFont_Load);
+            this.Resize += new System.EventHandler(this.FrmResizeTextboxAndFont_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.TextBox TxtSingle;
+        private System.Windows.Forms.TextBox TxtMultiline;
+    }
+}
+
---
+++ FrmResizeTextboxAndFont.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormResizeTextboxAndFonts
+{
+    public partial class FrmResizeTextboxAndFont : Form
+    {
+        ResizeFormControls ReSizeFrm;
+        public FrmResizeTextboxAndFont()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmResizeTextboxAndFont_Load(object sender, EventArgs e)
+        {
+            ReSizeFrm = new ResizeFormControls(this);
+        }
+
+        private void FrmResizeTextboxAndFont_Resize(object sender, EventArgs e)
+        {
+            ReSizeFrm.ResizeControls();
+        }
+    }
+
+
+    public class ResizeFormControls
+    {
+        Rectangle frmResizeInit;
+        Dictionary<string, Control> ctrls = new Dictionary<string, Control>();
+
+        private Form FrmToResize;
+
+        public ResizeFormControls(Form frm)
+        {
+            FrmToResize = frm;
+
+            frmResizeInit = new Rectangle(frm.ClientRectangle.X, frm.ClientRectangle.Y,
+                   frm.ClientRectangle.Width, frm.ClientRectangle.Height);
+            frm.Tag = frmResizeInit;
+
+            foreach (Control ctrl in frm.Controls)
+            {
+                ctrl.Tag = new Rectangle(ctrl.Left, ctrl.Top, ctrl.Width, ctrl.Height);
+                ctrls.Add(ctrl.Name, ctrl);
+
+                //if (ctrl.GetType() == typeof(TextBox)) { MessageBox.Show(((TextBox)ctrl).Multiline.ToString()); }
+            }
+
+        }
+
+        public void ResizeControls()
+        {
+            float xRatio = (float)FrmToResize.ClientRectangle.Width / (float)((Rectangle)FrmToResize.Tag).Width;
+            float yRatio = (float)FrmToResize.ClientRectangle.Height / (float)((Rectangle)FrmToResize.Tag).Height;
+
+            foreach (Control ctrl in FrmToResize.Controls)
+            {
+                ctrl.Left = Convert.ToInt32((((Rectangle)ctrls[ctrl.Name].Tag).Left) * xRatio);
+                ctrl.Top = Convert.ToInt32((((Rectangle)ctrls[ctrl.Name].Tag).Top) * yRatio);
+                ctrl.Width = Convert.ToInt32((((Rectangle)ctrls[ctrl.Name].Tag).Width) * xRatio);
+                ctrl.Height = Convert.ToInt32((((Rectangle)ctrls[ctrl.Name].Tag).Height) * yRatio);
+
+                //if (ctrl.GetType() == typeof(TextBox))
+                //{
+                //    ctrl.Top = ctrl.Top + (ctrl.Height / 2);
+                //}
+            }
+        }
+
+    }
+}
