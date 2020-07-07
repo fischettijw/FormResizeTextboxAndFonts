4e34106c84ffa7aabeb85ea1a2413018f661c5f1
Joseph Fischetti <fischettijw@gmail.com>
2020-07-07 06:12:48 -0400

    Inculded FONT Size

--- FrmResizeTextboxAndFont.cs
+++ FrmResizeTextboxAndFont.cs
@@ -47,7 +47,7 @@ namespace FormResizeTextboxAndFonts
 
             foreach (Control ctrl in frm.Controls)
             {
-                ctrl.Tag = new Rectangle(ctrl.Left, ctrl.Top, ctrl.Width, ctrl.Height);
+                ctrl.Tag = new InitCtrlInfo(ctrl.Left, ctrl.Top, ctrl.Width, ctrl.Height, ctrl.Font);
                 ctrls.Add(ctrl.Name, ctrl);
 
                 //if (ctrl.GetType() == typeof(TextBox)) { MessageBox.Show(((TextBox)ctrl).Multiline.ToString()); }
@@ -62,15 +62,34 @@ namespace FormResizeTextboxAndFonts
 
             foreach (Control ctrl in FrmToResize.Controls)
             {
-                ctrl.Left = Convert.ToInt32((((Rectangle)ctrls[ctrl.Name].Tag).Left) * xRatio);
-                ctrl.Top = Convert.ToInt32((((Rectangle)ctrls[ctrl.Name].Tag).Top) * yRatio);
-                ctrl.Width = Convert.ToInt32((((Rectangle)ctrls[ctrl.Name].Tag).Width) * xRatio);
-                ctrl.Height = Convert.ToInt32((((Rectangle)ctrls[ctrl.Name].Tag).Height) * yRatio);
+                ctrl.Left = Convert.ToInt32((((InitCtrlInfo)ctrls[ctrl.Name].Tag).Left) * xRatio);
+                ctrl.Top = Convert.ToInt32((((InitCtrlInfo)ctrls[ctrl.Name].Tag).Top) * yRatio);
+                ctrl.Width = Convert.ToInt32((((InitCtrlInfo)ctrls[ctrl.Name].Tag).Width) * xRatio);
+                ctrl.Height = Convert.ToInt32((((InitCtrlInfo)ctrls[ctrl.Name].Tag).Height) * yRatio);
+                //ctrl.Font = new Font((((InitCtrlInfo)ctrls[ctrl.Name].Tag).Font.FontFamily), (((InitCtrlInfo)ctrls[ctrl.Name].Tag).Font.Size) * yRatio);
 
-                //if (ctrl.GetType() == typeof(TextBox))
-                //{
-                //    ctrl.Top = ctrl.Top + (ctrl.Height / 2);
-                //}
+                if (ctrl.GetType() == typeof(TextBox))
+                {
+                    ctrl.Font = new Font((((InitCtrlInfo)ctrls[ctrl.Name].Tag).Font.FontFamily), (((InitCtrlInfo)ctrls[ctrl.Name].Tag).Font.Size) * yRatio);
+                }
+            }
+        }
+
+        public class InitCtrlInfo
+        {
+            public int Left;
+            public int Top;
+            public int Width;
+            public int Height;
+            public Font Font;
+
+            public InitCtrlInfo(int x, int y, int w, int h, Font f)
+            {
+                Left = x;
+                Top = y;
+                Width = w;
+                Height = h;
+                Font = f;
             }
         }
 
